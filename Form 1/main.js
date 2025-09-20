ef6a5aa9675e9bae9590f45c3c2598ddec0d3da6
J. Praveen Charles <praveencharles2806@gmail.com>
2025-09-20 19:52:51 +0530

    Add files via upload

---
+++ Form 1/main.js
@@ -0,0 +1,29 @@
+let Form = document.getElementById("form");
+
+Form.addEventListener("submit",function(e){
+
+    e.preventDefault();
+
+    let email = document.getElementById("Email").value.trim();
+    let Password = document.getElementById("Password").value.trim();
+
+    let EmailError = document.getElementById("EmailError");
+    let PasswordError = document.getElementById("PasswordError");
+
+    console.log(email);
+    console.log(Password);
+
+    if(email ==''){
+        EmailError.innerText = '*Required';
+    }else{
+        EmailError.innerText = '';
+    }
+
+    if(Password ==''){
+        PasswordError.innerText = '*Required';
+    }
+    else{
+        PasswordError.innerText = '';
+    }
+
+});
